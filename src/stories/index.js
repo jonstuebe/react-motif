fa4d50a13d82eaec549e75358f600d1a686d90ee
Jon Stuebe <jstuebe@gmail.com>
2018-01-07 06:28:12 -0700

    fix storybook errors

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as V from "victory";
-
+import styled from "styled-components";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 // import { linkTo } from "@storybook/addon-links";
@@ -22,7 +22,6 @@ import AppBar from "../components/AppBar";
 import Menu from "../components/Menu";
 import MenuItem from "../components/MenuItem";
 import MenuItemLink from "../components/MenuItemLink";
-
 import Tag from "../components/Tag";
 
 import { Table, Column, Cell } from "../components/Table";
@@ -31,75 +30,77 @@ import Chevron from "../icons/Chevron";
 
 import ProfilePhoto from "../components/ProfilePhoto";
 
+const StoryContainer = styled.div``;
+
 storiesOf("AppBar", module)
   .add("default", () => <AppBar left={"App"} />)
   .add("w/ right", () => (
-    <StoryContainer>
-      <AppBar
-        left={"App"}
-        right={
-          <ProfilePhoto
-            image="https://www.gravatar.com/avatar/ca43155a79bd234200f52ea50193c4b6?s=200"
-            style={{ marginTop: 5 }}
-          />
-        }
-      />
-    </StoryContainer>
-  );
+    <AppBar
+      left={"App"}
+      right={
+        <ProfilePhoto
+          image="https://www.gravatar.com/avatar/ca43155a79bd234200f52ea50193c4b6?s=200"
+          style={{ marginTop: 5 }}
+        />
+      }
+    />
+  ));
 
 storiesOf("MenuItem", module)
-  .add("default", () =>
+  .add("default", () => (
     <MenuItem
-      title="Investments"
-      description="Lists, reports, and information pertaining to disposition"
+      title="Menu Item"
+      description="Aenean Cursus Tellus Pellentesque Vulputate"
     />
-  )
-  .add("folder", () =>
+  ))
+  .add("folder", () => (
     <Router>
       <Menu>
         <MenuItem
-          title="Investments"
-          description="Lists, reports, and information pertaining to disposition"
+          title="Menu Item"
+          description="Aenean Cursus Tellus Pellentesque Vulputate"
         >
           <MenuItemLink
-            title="Dashboard"
-            description="Dashboard view of all current dispositions."
-            to="/investments/dashboard"
+            title="Fermentum Ipsum"
+            description="Tellus Mattis Pellentesque Vehicula Pharetra"
+            to="/purus/consectetur"
           />
           <MenuItemLink
-            title="List"
-            description="List view of all current dispositions."
-            to="/investments/list"
+            title="Lorem Dolor"
+            description="Inceptos Consectetur Condimentum Vulputate Purus."
+            to="/purus/wollis"
           />
           <MenuItemLink
-            title="List"
-            description="Reports view of all current dispositions."
-            to="/investments/reports"
+            title="Pharetra Pellentesque"
+            description="Vestibulum Venenatis Vehicula Mollis Cursus"
+            to="/purus/bibendum"
           />
         </MenuItem>
       </Menu>
     </Router>
-  )
-  .add("folder (active)", () =>
+  ))
+  .add("folder (active)", () => (
     <MenuItem
-      title="Investments"
-      description="Lists, reports, and information pertaining to disposition"
+      title="Menu Item"
+      description="Aenean Cursus Tellus Pellentesque Vulputate"
       folder
       active
     />
-  );
+  ));
 
 storiesOf("Button", module)
-  .add("default", () =>
+  .add("default", () => (
     <ThemeProvider>
       <Button onClick={action("clicked")}>Hello Button</Button>
     </ThemeProvider>
-  )
-  .add("primary", () =>
+  ))
+  .add("primary", () => (
     <ThemeProvider>
-      <Button onClick={action("clicked")} primary>Hello Button</Button>
+      <Button onClick={action("clicked")} primary>
+        Hello Button
+      </Button>
     </ThemeProvider>
-  );
+  ));
 
 storiesOf("Switch", module)
   .add("uncontrolled", () => <Switch onChange={action("changed")} />)
@@ -108,32 +109,28 @@ storiesOf("Switch", module)
   ))
   .add("uncontrolled w/ label", () => (
     <Switch onChange={action("changed")} label="Autosave" />
-  )
-  .add("default on", () =>
+  ))
+  .add("default on", () => (
     <Switch onChange={action("changed")} defaultValue={true} />
-  );
+  ));
 
 storiesOf("Checkbox", module)
   .add("uncontrolled", () => <Checkbox onChange={action("changed")} />)
-  .add("uncontrolled w/ label", () =>
+  .add("uncontrolled w/ label", () => (
     <Checkbox onChange={action("changed")} label="Select All" />
-  )
-  .add("default on", () =>
+  ))
+  .add("default on", () => (
     <Checkbox onChange={action("changed")} defaultValue={true} />
-  );
+  ));
 
-storiesOf("Tag", module).add("default", () => <Tag>Vacant Unrented Ready</Tag>);
+storiesOf("Tag", module).add("default", () => <Tag>Quam Nullam Malesuada</Tag>);
 
 storiesOf("Table", module).add("basic", () => {
   let list = [];
-  const statuses = [
-    "Vacant Unrented Not Ready",
-    "Notice Unrented Not Ready",
-    "Notice Unrented Ready"
-  ];
+  const statuses = ["Vacant", "Under Renovation", "On The Market"];
   for (var i = 0; i < 15; i++) {
     list.push({
-      propertyId: faker.random.number(),
+      homeId: faker.random.number(),
       streetAddress: faker.address.streetAddress(),
       city: faker.address.city(),
       state: faker.address.stateAbbr(),
@@ -144,18 +141,30 @@ storiesOf("Table", module).add("basic", () => {
 
   return (
     <Table data={list} striped hover>
-      <Column dataKey="propertyId" width={115}>Property ID</Column>
-      <Column dataKey="streetAddress" width={225}>Street Address</Column>
-      <Column dataKey="city" width={135}>City</Column>
-      <Column dataKey="state" width={55}>State</Column>
-      <Column dataKey="zip" width={100}>Zip</Column>
+      <Column dataKey="homeId" width={115}>
+        Property ID
+      </Column>
+      <Column dataKey="streetAddress" width={225}>
+        Street Address
+      </Column>
+      <Column dataKey="city" width={135}>
+        City
+      </Column>
+      <Column dataKey="state" width={55}>
+        State
+      </Column>
+      <Column dataKey="zip" width={100}>
+        Zip
+      </Column>
       <Column
         dataKey="status"
         width={225}
-        cellRenderer={({ label, value }) =>
+        cellRenderer={({ label, value }) => (
           <Cell>
-            <span className="mobile-label">{label}</span><Tag>{value}</Tag>
-          </Cell>}
+            <span className="mobile-label">{label}</span>
+            <Tag>{value}</Tag>
+          </Cell>
+        )}
       >
         Status
       </Column>
@@ -166,9 +175,9 @@ storiesOf("Table", module).add("basic", () => {
 storiesOf("Icons", module)
   .add("chevron right small", () => <Chevron direction="right" />)
   .add("chevron left small", () => <Chevron direction="left" />)
-  .add("chevron right medium", () =>
+  .add("chevron right medium", () => (
     <Chevron direction="right" size="medium" />
-  )
+  ))
   .add("chevron left medium", () => <Chevron direction="left" size="medium" />)
   .add("chevron right large", () => <Chevron direction="right" size="large" />)
   .add("chevron left large", () => <Chevron direction="left" size="large" />);
